7059a5d17c17446920955f188d9d87e98300e793
Santiago Ariel <santiagoarielv98@gmail.com>
2023-11-07 16:52:20 -0300

    Refactored routes and added controllers for genre, author, book, and bookinstance

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var wikiRouter = require("./routes/wiki");
+var catalogRouter = require("./routes/catalog");
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/wiki", wikiRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

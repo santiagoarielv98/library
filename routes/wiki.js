c8514f729c5cb2860e9a9b04f106efa02fd18991
Santiago Ariel <santiagoarielv98@gmail.com>
2023-11-07 16:46:34 -0300

    Refactor about route to use async/await

--- routes/wiki.js
+++ routes/wiki.js
@@ -11,13 +11,13 @@ router.get("/about", function (req, res) {
   res.send("About this wiki");
 });
 
-router.get("/about", (req, res, next) => {
-  About.find({}).exec((err, queryResults) => {
-    if (err) {
-      return next(err);
-    }
-    res.render("about_view", { title: "About", list: queryResults });
-  });
+exports.get("/about", async function (req, res, next) {
+  try {
+    const successfulResult = await About.find({}).exec();
+    res.render("about_view", { title: "About", list: successfulResult });
+  } catch (error) {
+    return next(error);
+  }
 });
 
 module.exports = router;

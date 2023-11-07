ee3e52bdbfcff0499b71cfcec074b38618083b19
Santiago Ariel <santiagoarielv98@gmail.com>
2023-11-07 16:46:26 -0300

    Add express-async-handler package and update about route in wiki.js

--- routes/wiki.js
+++ routes/wiki.js
@@ -11,4 +11,13 @@ router.get("/about", function (req, res) {
   res.send("About this wiki");
 });
 
+router.get("/about", (req, res, next) => {
+  About.find({}).exec((err, queryResults) => {
+    if (err) {
+      return next(err);
+    }
+    res.render("about_view", { title: "About", list: queryResults });
+  });
+});
+
 module.exports = router;

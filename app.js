470620bb3e5e6095a8c02cb775b00c7b3c310934
Santiago Ariel <santiagoarielv98@gmail.com>
2023-11-08 10:52:28 -0300

    Add compression, rate limiting, and helmet middleware. Update express and pug dependencies.

--- app.js
+++ app.js
@@ -3,17 +3,33 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+const compression = require("compression");
+const RateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var catalogRouter = require("./routes/catalog");
 
 var app = express();
 
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(limiter);
+app.use(compression());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
